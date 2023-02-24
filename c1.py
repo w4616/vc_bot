5190413ecb73ca0b01e11be9fac066e86b80ba6c
qianjiu <90247377+w4616@users.noreply.github.com>
2023-02-24 16:30:52 +0800

    Update c1.py

--- c1.py
+++ c1.py
@@ -10,7 +10,7 @@ cookies = {
     'PHPSESSID': PHPSESSID1,
 }
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.22 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
     'Referer': 'https://free.vps.vc/vps-info',
