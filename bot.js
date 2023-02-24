25ea5483895fe4b8d929b2594256d4ac9ef485a4
qianjiu <90247377+w4616@users.noreply.github.com>
2023-02-24 16:26:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ bot.onText(/\/start/, (msg) => {
 // 处理 /help 命令
 bot.onText(/\/help/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, '/vc 库存\n/x 系统没什么实际作用\n/ping 检测是否存货\n/ck 提交ck（未写）\n/help 帮助');
+  bot.sendMessage(chatId, '/vc 库存\n/x 系统 没什么实际作用\n/ping 检测是否存货\n/ck 提交ck\n/help 帮助');
 });
 
 // 处理 /ping 命令
